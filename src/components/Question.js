2d4a8d4240c47c4586ed9fcbbdf1ea8f4f9882a0
Alexander Widmann <30512088+ALKW@users.noreply.github.com>
2022-12-30 22:37:38 -0700

    Add support for youtube video and image questions

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,36 @@
 import React from 'react';
 
 // Made into a pure function, as class notation was not necessary
-export default props => (
-  <div className='question'>{props.question.question}</div>
-);
+
+function toEmbededLink(youtubeLink) {
+  return "https://www.youtube.com/embed/" + getYoutubeVideoId(youtubeLink);
+}
+
+function getYoutubeVideoId(url) {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+
+  return (match && match[2].length === 11)
+    ? match[2]
+    : null;
+}
+
+export default props => {
+  var questionType = "text";
+  if (props.question.question) {
+    questionType = "text";
+  } else if (props.question.youtubeLink) {
+    questionType = "video";
+    props.question.youtubeLink = toEmbededLink(props.question.youtubeLink);
+    console.log(`embeded link: ${toEmbededLink(props.question.youtubeLink)}`);
+  } else if (props.question.imageLink) {
+    questionType = "image";
+  }
+  return (
+    <div className='question'>
+      {questionType === "text" && <span>{props.question.question}</span>}
+      {questionType === "video" && <iframe width="960" height="540" src={props.question.youtubeLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
+      {questionType === "image" && <img src={props.question.imageLink} alt="Your Link is Fucked Up dude"></img>}
+    </div>
+  );
+}
